fdaadba76509184ab64af16300196731c3fdabd3
sureshbabuvikram <sureshbabuvikram@gmail.com>
2024-08-23 20:14:57 +0530

    first commit

---
+++ Readme.md
@@ -0,0 +1,5 @@
+this program is used to know about functions
+
+Admin Credentials
+username:suresh
+password:suresh@123
---
+++ script.js
@@ -0,0 +1,15 @@
+function name() {
+    console.log("log");    
+}
+
+function name2(){
+    console.log("name2");
+    
+}
+
+function name3(){
+    console.log("name3");
+    
+}
+
+
